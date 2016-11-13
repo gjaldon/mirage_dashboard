188ac5ab5ef298c44fea8176eb6c7ee59a28bcf6
Gabriel Jaldon <gjaldon85@gmail.com>
2016-11-14 03:24:27 +0800

    testing js_of_ocaml

--- Makefile
+++ Makefile
@@ -1,5 +1,11 @@
 build:
 	ocamlbuild -use-ocamlfind main.byte src/main.ml
 
+client: _build/js/client.js
+
+_build/js/client.js:
+	ocamlbuild -use-ocamlfind client.byte js/client.ml
+	js_of_ocaml +weak.js _build/js/client.byte
+
 clean:
 	ocamlbuild -clean
--- src/main.ml
+++ src/main.ml
@@ -1,10 +1,26 @@
 open Opium.Std
+open Tyxml.Html
 
-let print_param = get "/:name" (fun req ->
-    let name = param req "name" in
-    `String ("Hello " ^ name) |> respond')
+let index_page =
+  "<!DOCTYPE html>
+  <html class=\"no-js\" lang=\"en\">
+    <head>
+      <meta charset=\"utf-8\">
+      <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" />
+      <title>Mirage-Dashboard</title>
+    </head>
+    <body>
+      <div id='app'></div>
+      <script type=\"text/javascript\" src=\"assets/client.js\"></script>
+    </body>
+  </html>"
+
+
+let home_page = get "/" (fun req ->
+    `Html index_page |> respond')
 
 let _ =
   App.empty
-  |> print_param
+  |> middleware (Middleware.static ~local_path:"./_build/js/" ~uri_prefix:"/assets")
+  |> home_page
   |> App.run_command
