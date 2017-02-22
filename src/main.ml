efdddd06d63fdb77b832dcde2dc9e2a33ef4539b
Gabriel Jaldon <gjaldon85@gmail.com>
2017-02-22 14:49:54 +0800

    Add projects endpoint with hardcoded json data

--- src/main.ml
+++ src/main.ml
@@ -1,4 +1,5 @@
 open Opium.Std
+open Opium_misc
 open Tyxml.Html
 
 module Server = Cohttp_lwt_unix.Server
@@ -9,8 +10,17 @@ let home_page = get "/" (fun req ->
     Response.of_response_body resp
   )
 
+let projects = get "/projects" (fun req ->
+    let headers = Cohttp.Header.init_with "content-type" "application/json" in
+    let json = `String "{name: \"Gabriel\"}" in
+    Server.respond ~headers ~status:`OK ~body:json () >>| fun resp ->
+    Response.of_response_body resp
+  )
+
 let _ =
-  App.empty
+  let app = App.empty in
+  app
   |> middleware (Middleware.static ~local_path:"./_build/js/" ~uri_prefix:"/assets")
   |> home_page
+  |> projects
   |> App.run_command
