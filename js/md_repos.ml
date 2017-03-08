defb26abbeb19e044fffa2bdb9e159523450effb
Gabriel Jaldon <gjaldon85@gmail.com>
2017-03-09 00:20:40 +0800

    Show repo details page

--- js/md_repos.ml
+++ js/md_repos.ml
@@ -1,10 +1,12 @@
 open React
 open Lwt.Infix
+open Github_t
 
 type pagination = {
-  repos: Github_t.repositories;
+  repos: repositories;
   total_pages: int;
   current_page: int;
+  selected_repo: repository option;
 }
 
 module Model = struct
@@ -18,6 +20,7 @@ module Model = struct
       repos = Util.member "repos" json |> to_string |> Github_j.repositories_of_string;
       total_pages = Util.member "total_pages" json |> Util.to_int;
       current_page = Util.member "current_page" json |> Util.to_int;
+      selected_repo = None;
     }
 
   let update = function
@@ -26,9 +29,14 @@ module Model = struct
         repos = [];
         total_pages = 0;
         current_page = 1;
+        selected_repo = None;
       }
     | `Loaded content ->
       json_to_pagination content
+    | `Selected (record, repo) ->
+      { record with selected_repo = repo }
+    | `List record ->
+      { record with selected_repo = None }
 
   let display =
     S.l1 update state
@@ -38,6 +46,17 @@ module Model = struct
     XmlHttpRequest.get repos_path >|= fun resp ->
     set_state (`Loaded resp.content)
 
+  let select_repo record id =
+    let find_repo () =
+      try Some (List.find (fun repo -> compare repo.repository_id id = 0) record.repos) with
+      | Not_found -> None
+    in
+    let repo = find_repo () in
+    set_state (`Selected (record, repo))
+
+  let list_repos record =
+    set_state (`List record)
+
   let _ =
     get_repos_by_page 1
 end
@@ -45,24 +64,29 @@ end
 module R = Tyxml_js.R.Html5
 open Tyxml_js.Html5
 
-let render () =
+let onclick_repo record repo_id =
+  Printf.printf "Selected repo id: %s" (Int64.to_string repo_id);
+  a_onclick (fun _ev -> Model.select_repo record repo_id; true)
+
+let render_repos () =
   Model.display
   |> S.map (function
       | {repos = []; _} ->
         pcdata "None"
-      | {repos; _} ->
+      | {repos; _} as record ->
         ul (List.map (fun repo ->
-          li [pcdata repo.Github_j.repository_name]
+          li ~a:[onclick_repo record repo.repository_id] [pcdata repo.repository_name]
         ) repos)
     )
   |> ReactiveData.RList.singleton_s
   |> R.div
 
 let page_onclick page =
-  a_onclick (fun _ev -> Model.get_repos_by_page page; true)
+  a_onclick (fun _ev -> Model.get_repos_by_page page |> ignore; true)
+
+open Md_util
 
 let render_pagination () =
-  let open Md_util in
   Model.display
   |> S.map (fun pagination ->
       let {total_pages; current_page; _} = pagination in
@@ -75,6 +99,39 @@ let render_pagination () =
           ) (1--total_pages)
       in
       ul ~a:[a_class ["pagination"]] list_items
+    )
+  |> ReactiveData.RList.singleton_s
+  |> R.div
+
+let back_onclick record =
+  a_onclick (fun _ev -> Model.list_repos record |> ignore; true)
+
+let render () =
+  Model.display
+  |> S.map (fun record ->
+      let {selected_repo; _} = record in
+      match selected_repo with
+      | None ->
+        div [
+          render_repos ();
+          render_pagination ();
+        ]
+      | Some repo ->
+        div [
+          h2 [pcdata (Printf.sprintf "Repo: %s" repo.repository_full_name)];
+          dl [
+            dt [pcdata "Description"];
+            dd [opt_with_default repo.repository_description "N/A" |> pcdata];
+            dt [pcdata "Repo URL"];
+            dd [pcdata repo.repository_html_url];
+            dt [pcdata "Forks"];
+            dd [string_of_int repo.repository_forks_count |> pcdata];
+            dt [pcdata "Stars"];
+            dd [string_of_int repo.repository_stargazers_count |> pcdata];
+          ];
+          a ~a:[back_onclick record; a_href "#"] [pcdata "Back"];
+        ]
+
     )
   |> ReactiveData.RList.singleton_s
   |> R.div
--- js/md_util.ml
+++ js/md_util.ml
@@ -4,3 +4,9 @@ let (--) i j =
     if n < i then acc else aux (n-1) (n :: acc)
   in
   aux j []
+
+let opt_with_default opt default =
+  match opt with
+  | None -> default
+  | Some a -> a
+
